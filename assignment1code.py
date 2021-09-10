4564dbd93c4fadfee6d4614224f016c4a54278c1
polachandu <86178027+polachandu@users.noreply.github.com>
2021-09-10 09:58:03 -0600

    Add files via upload

---
+++ assignment1code.py
@@ -0,0 +1,8 @@
+# ENSF 592 Spring 2021
+# Assignment 1 Starter Code
+# Chandrahas Reddy Pola
+
+studentName = "Chandrahas Reddy Pola"  # Change the contents of this variable to your name (leave the quotation marks).
+
+print("Hello, World!")
+print("My name is " + studentName + " and I am a student in ENSF 592.")
